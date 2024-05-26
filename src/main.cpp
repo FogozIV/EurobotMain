e953fe9259046f62c2ec0d66a6074719a6da2fe0
FogozIV <fogoziv@gmail.com>
2024-05-27 00:51:45 +0200

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "packet/Import.h"
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    auto data = (packet_raw_type )malloc(65536);
+    ClientBoundPingPacket packet;
+    packet_size_type  size = packet.getFullPacket(data);
+    std::cout << size << std::endl;
+    std::shared_ptr<BasePacket> basePacket = ClientBoundPingPacket::parsePacket(data);
+    std:: cout << basePacket->getPacketID()<< std::endl;
+    PacketParser parser;
+    return 0;
+}
